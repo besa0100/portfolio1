acf529314005fd232f4497bea03d83527625504d
Besart Bara <besartbara@Besarts-Air.lan>
2019-06-17 23:03:13 +0200

    tisdag

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
+/*burger menu*/
 window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("sidenVises");
-
     document.querySelector("#menuknap").addEventListener("click", toggleMenu);
-
-
 }
-
 
 function toggleMenu() {
     console.log("toggleMenu");
     document.querySelector("#menu").classList.toggle("hidden");
 
     let erSkjult = document.querySelector("#menu").classList.contains("hidden");
-
     if (erSkjult == true) {
         document.querySelector("#menuknap").textContent = "☰";
-        document.querySelector("#header").classList.remove("farve");
-        document.querySelector("#menu").classList.remove("farve");
-
     } else {
         document.querySelector("#menuknap").textContent = "X";
-        document.querySelector("#header").classList.add("farve");
-        document.querySelector("#menu").classList.add("farve");
-
     }
 }
